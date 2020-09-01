0f2b67097217c7ccd09ab2dbde4037d505b723c0
imamaja1 <imam.aja0022@gmail.com>
2020-09-01 13:43:22 +0800

    edit dashboatd spbe

--- application/views/SPBE/dashboard.php
+++ application/views/SPBE/dashboard.php
@@ -14,6 +14,107 @@
     <!-- CSS INCLUDE -->
     <link rel="stylesheet" type="text/css" id="theme" href="<?= base_url() ?>assets/css/theme-default.css" />
     <!-- EOF CSS INCLUDE -->
+    <style>
+        .timeline2 {
+            position: relative;
+            padding: 21px 0px 10px;
+            margin-top: 4px;
+            margin-bottom: 30px;
+        }
+
+        .timeline2 .line {
+            position: absolute;
+            width: 4px;
+            display: block;
+            background: currentColor;
+            top: 0px;
+            bottom: 0px;
+            margin-left: 30px;
+        }
+
+        .timeline2 .separator {
+            border-top: 1px solid currentColor;
+            padding: 5px;
+            padding-left: 40px;
+            font-style: italic;
+            font-size: .9em;
+            margin-left: 30px;
+        }
+
+        .timeline2 .line::before {
+            top: -4px;
+        }
+
+        .timeline .line::after {
+            bottom: -4px;
+        }
+
+        .timeline2 .line::before,
+        .timeline2 .line::after {
+            content: '';
+            position: absolute;
+            left: -4px;
+            width: 12px;
+            height: 12px;
+            display: block;
+            border-radius: 50%;
+            background: currentColor;
+        }
+
+        .timeline2 .panel {
+            position: relative;
+            margin: 10px 0px 21px 70px;
+            clear: both;
+        }
+
+        .timeline2 .panel::before {
+            position: absolute;
+            display: block;
+            top: 8px;
+            left: -24px;
+            content: '';
+            width: 0px;
+            height: 0px;
+            border: inherit;
+            border-width: 12px;
+            border-top-color: transparent;
+            border-bottom-color: transparent;
+            border-left-color: transparent;
+        }
+
+        .timeline2 .panel .panel-heading.icon * {
+            font-size: 20px;
+            vertical-align: middle;
+            line-height: 40px;
+        }
+
+        .timeline2 .panel .panel-heading.icon {
+            position: absolute;
+            left: -59px;
+            display: block;
+            width: 40px;
+            height: 40px;
+            padding: 0px;
+            border-radius: 50%;
+            text-align: center;
+            float: left;
+        }
+
+        .timeline2 .panel-outline {
+            border-color: transparent;
+            background: transparent;
+            box-shadow: none;
+        }
+
+        .timeline2 .panel-outline .panel-body {
+            padding: 10px 0px;
+        }
+
+        .timeline2 .panel-outline .panel-heading:not(.icon),
+        .timeline2.panel-outline .panel-footer {
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
@@ -102,6 +203,33 @@
         </div>
     </div>
 
+    <div class="modal fade" id="timeline" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="timeline2">
+                                <div class="line text-muted"></div>
+                                <!-- <div class="separator text-muted">
+                                    <time>Time Line</time>
+                                </div> -->
+                                <article class="panel panel-info panel-outline"></article>
+                                <div id="datatimeline"></div>
+                                <div class="separator text-muted"></div>
+                                <article class="panel panel-info panel-outline"></article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary pull-right" data-dismiss="modal" style="margin: 0 0 0 0;">close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <!-- START PRELOADS -->
@@ -129,6 +257,42 @@
         var kode_permintaan;
         var tgl;
         var id;
+
+        function kode3(kode, jum) {
+            document.getElementById('datatimeline').innerHTML = null;
+            $.ajax({
+                type: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': "Basic " + btoa("gas:gas")
+                },
+                url: " <?= base_url() ?>Rest_API/permintaan?KEY-SPBE=SPBE&id=" + kode,
+                contentType: "application/json",
+                dataType: 'json',
+                success: function(response) {
+                    // document.getElementById('namatimeline').innerHTML = response.data[0].nama_spbe;
+                    if (jum > 1) {
+                        document.getElementById('datatimeline').innerHTML += ' <div class="separator text-muted"><time>' + response.data[0].tgl + '</time></div><article class="panel panel-info panel-outline"><div class="panel-heading icon"><i class="glyphicon glyphicon-info-sign"></i></div><div class="panel-body"><strong>Pemintaan Skid Tank</strong> dari <strong>SPBE</strong>.</div></article>';
+                        if (jum > 2) {
+                            document.getElementById('datatimeline').innerHTML += ' <div class="separator text-muted"><time>' + response.data[0].tgl_berangkat_tujuan + '</time></div><article class="panel panel-info panel-outline"><div class="panel-heading icon"><i class="glyphicon glyphicon-info-sign"></i></div><div class="panel-body"><strong>Verifikasi Perminaan Skid Tank</strong> <br>oleh <strong>Patra Niaga</strong>.</div></article>';
+                            if (jum > 3) {
+                                document.getElementById('datatimeline').innerHTML += ' <div class="separator text-muted"><time>' + response.data[0].tgl_berangkat_tujuan + '</time></div><article class="panel panel-info panel-outline"><div class="panel-heading icon"><i class="glyphicon glyphicon-info-sign"></i></div><div class="panel-body"> Proses <strong>Skid Tank Berangkat</strong>.</div></article>';
+                                if (jum > 4) {
+                                    document.getElementById('datatimeline').innerHTML += ' <div class="separator text-muted"><time>' + response.data[0].tgl_sampai_tujuan + '</time></div><article class="panel panel-info panel-outline"><div class="panel-heading icon"><i class="glyphicon glyphicon-info-sign"></i></div><div class="panel-body">  <strong>Skid Tank telah sampai</strong> di tujuan.</div></article>';
+                                    if (jum > 5) {
+                                        document.getElementById('datatimeline').innerHTML += ' <div class="separator text-muted"><time>' + response.data[0].tgl_kembali + '</time></div><article class="panel panel-info panel-outline"><div class="panel-heading icon"><i class="glyphicon glyphicon-info-sign"></i></div><div class="panel-body">  <strong>Skid Tank Kembali</strong> Ke Pangkalan.</div></article>';
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            })
+
+
+        }
+
         $.ajax({
             type: 'GET',
             headers: {
@@ -188,18 +352,18 @@
                         var times = moment(x.getFullYear() + '-' + (x.getMonth() + 1) + '-' + x.getDate() + ' ' + x.getHours() + ':' + x.getMinutes() + ':' + x.getSeconds());
                         $.each(response.data, function(i) {
                             if (response.data[i].status_permintaan != '2') {
-                                if (response.data[i].status_terminal == '2') {
-                                    if (response.data[i].status_patra_niaga == '2') {
-                                        if (response.data[i].tgl_berangkat_tujuan < response.data[i].tgl_sampai_tujuan) {
-                                            document.getElementById('progres').innerHTML += '<div class="progress-list"><div class="pull-left"><strong>Skid Tank dengan NOPOL ' + response.data[i].nopol + '</strong></div><br><div class="pull"><span class="text-info">Telah Sampai Ditujuan</span></div><div class="pull-right">100%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div></div></div>'
+                                if (response.data[i].status_patra_niaga == '2') {
+                                    if (moment(response.data[i].tgl_berangkat_tujuan) < times) {
+                                        if (moment(response.data[i].tgl_sampai_tujuan) < times && moment(response.data[i].tgl_berangkat_tujuan) < moment(response.data[i].tgl_sampai_tujuan)) {
+                                            document.getElementById('progres').innerHTML += '<div class="progress-list"><button class="btn btn-primary btn-rounded pull-right" data-toggle="modal" data-target="#timeline" onclick="kode3(' + response.data[i].kode_permintaan + ',4)">Time Line</button><div class="pull-left"><strong>' + response.data[i].nama_spbe + '</strong></div><br><div class="pull"><span class="text-info">Skdi Tank di Tujuan</span></div><div class="pull-right">100%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div></div></div>'
                                         } else {
-                                            document.getElementById('progres').innerHTML += '<div class="progress-list"><div class="pull-left"><strong>' + response.data[i].nopol + '</strong></div><br><div class="pull"><span class="text-info">Proses Perjalan Menuju Tujuan</span></div><div class="pull-right">70%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">70%</div></div></div>'
+                                            document.getElementById('progres').innerHTML += '<div class="progress-list"><button class="btn btn-primary btn-rounded pull-right" data-toggle="modal" data-target="#timeline" onclick="kode3(' + response.data[i].kode_permintaan + ',4)">Time Line</button><div class="pull-left"><strong>' + response.data[i].nama_spbe + '</strong></div><br><div class="pull"><span class="text-info">Proses Perjalan Menuju Tujuan</span></div><div class="pull-right">70%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">70%</div></div></div>'
                                         }
-                                    } else if (response.data[i].status_patra_niaga == '1') {
-                                        document.getElementById('progres').innerHTML += '<div class="progress-list"><div class="pull-left"><strong>' + response.data[i].nopol + '</strong></div><br><div class="pull"><span class="text-info">Permintaan Verifikasi Patra Niaga</span></div><div class="pull-right">40%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">40%</div></div></div>'
+                                    } else {
+                                        document.getElementById('progres').innerHTML += '<div class="progress-list"><button class="btn btn-primary btn-rounded pull-right" data-toggle="modal" data-target="#timeline" onclick="kode3(' + response.data[i].kode_permintaan + ',3)">Time Line</button><div class="pull-left"><strong>' + response.data[i].nama_spbe + '</strong></div><br><div class="pull"><span class="text-info">Proses Berangakt Menuju Tujuan</span></div><div class="pull-right">20%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div></div></div>'
                                     }
-                                } else if (response.data[i].status_terminal == '1') {
-                                    document.getElementById('progres').innerHTML += '<div class="progress-list"><div class="pull-left"><strong>' + response.data[i].nopol + '</strong></div><br><div class="pull"><span class="text-info">Permintaan Verifikasi Terminal</span></div><div class="pull-right">20%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">20%</div></div></div>'
+                                } else if (response.data[i].status_patra_niaga == '1') {
+                                    document.getElementById('progres').innerHTML += '<div class="progress-list"><button class="btn btn-primary btn-rounded pull-right" data-toggle="modal" data-target="#timeline" onclick="kode3(' + response.data[i].kode_permintaan + ',2)">Time Line</button><div class="pull-left"><strong>' + response.data[i].nama_spbe + '</strong></div><br><div class="pull"><span class="text-info">Permintaan Verifikasi Patra Niaga</span></div><div class="pull-right">30%</div><div class="progress progress-small progress-striped active"><div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">30%</div></div></div>'
                                 }
                             }
                         });
